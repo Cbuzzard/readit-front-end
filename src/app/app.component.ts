8913f39eca287b575627e43f846daad895d83ea0
cbuzzard <corwin1998@gmail.com>
2020-08-16 21:19:06 -0400

    added background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'readit-front-end';
+  background;
+
+  @HostListener('document:mousemove', ['$event']) 
+  onMouseMove(e) {
+    this.background.style.backgroundPositionX = -e.offsetX/40 + "px";
+    this.background.style.backgroundPositionY = -e.offsetY/30 + "px";
+    console.log(e);
+  }
+
+  constructor(private elementRef: ElementRef){}
+
+  ngAfterViewInit(): void {
+    this.background = this.elementRef.nativeElement.querySelector('#background');
+  }
+
 }
